00217b34c1a4962bf31239d0627f73fb862a8639
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-27 10:12:27 -0700

    Created basic function to get screenshot of example.com website and save it to server root as example.png.

--- snapshotjs-server/controllers/indexController.js
+++ snapshotjs-server/controllers/indexController.js
@@ -1,6 +1,16 @@
+const puppeteer = require('puppeteer');
+
 module.exports = {
   //Get Index
   getIndex: function(req, res) {
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto('https://www.example.com');
+      await page.screenshot({ path: 'example.png' });
+      await browser.close();
+    })();
+
     res.status(200).json({
       message: "Welcome to the backend!"
     });
