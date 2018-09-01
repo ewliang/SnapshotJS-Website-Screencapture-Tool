583b8928f43a72293e6cce21f028d956f147cea5
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-08-31 22:51:57 -0700

    Initial commit.

---
+++ snapshotjs-client/components/MainFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <span>Copyright &copy; 2018 Eric Liang. Designed and Developed by Eric Liang. All Rights Reserved.</span>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ snapshotjs-client/components/URLInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <input type = "url" placeholder = "Type a URL here (e.g. https://www.example.com)">
+    <input type = "submit" value = "Take Screenshot">
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
