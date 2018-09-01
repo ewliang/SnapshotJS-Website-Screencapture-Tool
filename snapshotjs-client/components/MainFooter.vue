bc23b19664ef14c6e910c5ca0466b9209d12b182
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-01 10:50:40 -0700

    Added link.

--- snapshotjs-client/components/MainFooter.vue
+++ snapshotjs-client/components/MainFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <span>Copyright &copy; 2018 Eric Liang. Designed and Developed by Eric Liang. All Rights Reserved.</span>
+    <span>Copyright &copy; 2018 Eric Liang. Designed and Developed by <a href = "https://www.eric-liang.com">Eric Liang</a>. All Rights Reserved.</span>
   </footer>
 </template>
 
