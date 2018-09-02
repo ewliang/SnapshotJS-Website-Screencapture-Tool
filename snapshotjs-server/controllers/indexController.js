1fa993ea640335b20c85bee3a5a6a16b1a87ee77
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-01 23:46:13 -0700

    Refactored some console.log and added URL req for dynamic URL input from client.

--- snapshotjs-server/controllers/indexController.js
+++ snapshotjs-server/controllers/indexController.js
@@ -3,14 +3,15 @@ const puppeteer = require('puppeteer');
 module.exports = {
   //Get Index
   getIndex: function(req, res) {
-    var url = 'https://google.com';
+    var url = req.body.URL;
     var screenshotFileName = 'untitled';
     var fileFormat = '.jpg';
     (async () => {
       const browser = await puppeteer.launch({ headless: true });
       const page = await browser.newPage();
       await page.goto(url, { waitUntil: 'load', timeout: 300000 });
-      console.log('Setting up viewport and awaiting content load...');
+      console.log('Setting up viewport.');
+      console.log('Loading content.');
       await page.setViewport({ width: 1920, height: 1080 });
       // Incremental Scroll Page To Load Sections Of Content
       // Test: www.latimes.com - Long page and lazy load.
@@ -31,19 +32,21 @@ module.exports = {
           }
         })();
       });
+      console.log('Waiting for lazy loaded content to appear...');
       await page.waitFor(30000); // 30,000 confirmed = 30 sec
       console.log('Taking screenshot...');
       await page.screenshot({ path: screenshotFileName + fileFormat, fullPage: true });
       console.log('Finished taking screenshot!');
       await browser.close();
+      // Send screenshot image file to client.
+      console.log('Sending file to client.');
       await res.sendFile(screenshotFileName + fileFormat, { 'root': './' }, function (err) {
         if (!err) {
-          console.log('Sent:', screenshotFileName);
+          console.log('File Sent: ' + screenshotFileName + fileFormat);
         } else {
           console.log(err);
         }
       });
-
     })();
     /*
     res.status(200).json({
