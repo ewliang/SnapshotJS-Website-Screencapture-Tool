c8c2c2862d9962a0f2cf2a13b750bafe32e9e6c0
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-01 23:45:29 -0700

    created method to call screenshot API route and generate download link for image response.

--- snapshotjs-client/components/URLInput.vue
+++ snapshotjs-client/components/URLInput.vue
@@ -1,13 +1,39 @@
 <template>
-  <form>
-    <input type = "url" placeholder = "Type a URL here (e.g. https://www.example.com)">
+  <form @submit.prevent = "captureScreenshot()">
+    <input type = "url" v-model = "URL" placeholder = "Type a URL here (e.g. https://www.example.com)">
     <input type = "submit" value = "Take Screenshot">
   </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  data () {
+    return {
+      URL: ''
+    }
+  },
+  methods: {
+    captureScreenshot () {
+      let URL = {
+        URL: this.URL
+      }
+      axios.post('http://localhost:4000/', URL, {
+        responseType: 'arraybuffer'
+      })
+      .then(function(response) {
+        let blob = new Blob([response.data], { type: 'image/jpeg' })
+        let link = document.createElement('a')
+        link.href = window.URL.createObjectURL(blob)
+        let downloadElement = document.createTextNode('Download')
+        link.appendChild(downloadElement)
+        document.body.appendChild(link)
+        // link.download = 'screenie.jpg'
+        // link.click()
+      })
+    }
+  }
 }
 </script>
 
