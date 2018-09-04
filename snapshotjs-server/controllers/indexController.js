405dd817a11576b0ed7649d369639a154273599d
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-03 19:34:31 -0700

    Refactored some code for improved performance. Removed old res.json() 200 response that was commented. Improved delay time by using if statement to determine if fullPage or not. If fullPage, do scroll interval and lazy load 30sec wait time, else skip scroll and do 3 sec wait time.

--- snapshotjs-server/controllers/indexController.js
+++ snapshotjs-server/controllers/indexController.js
@@ -3,13 +3,13 @@ const puppeteer = require('puppeteer');
 module.exports = {
   //Get Index
   getIndex: function(req, res) {
-    var url = req.body.URL;
-    var fullPage = req.body.fullPage;
-    var screenshotFileName = 'untitled';
-    var fileFormat = '.jpg';
-    var viewWidth = req.body.width;
-    var viewHeight = req.body.height;
     (async () => {
+      var url = req.body.URL;
+      var fullPage = req.body.fullPage;
+      var screenshotFileName = 'untitled';
+      var fileFormat = '.jpg';
+      var viewWidth = req.body.width;
+      var viewHeight = req.body.height;
       console.log('Capturing: ' + url + '\n' + 'Full Page: ' + fullPage + '\n' + viewWidth + 'x' + viewHeight);
       const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless: true });
       const page = await browser.newPage();
@@ -19,25 +19,28 @@ module.exports = {
       await page.setViewport({ width: viewWidth, height: viewHeight });
       // Incremental Scroll Page To Load Sections Of Content
       // Test: www.latimes.com - Long page and lazy load.
-      await page.evaluate(() => {
-        let screenHeight = document.documentElement.clientHeight;
-        let scrollHeight = document.body.scrollHeight;
-        let screenHeightScrollIncrements = Math.ceil(scrollHeight / screenHeight);
-        var tempScrolledAmount = screenHeight;
-        (function() {
-          var counter = 0;
-          var myVar = setInterval(myTimer, 3000);
-          function myTimer() {
-              window.scrollBy(0, screenHeight);
-              counter++;
-              if(counter > screenHeightScrollIncrements) {
-                clearInterval(myVar);
-              }
-          }
-        })();
-      });
-      console.log('Waiting for lazy loaded content to appear...');
       if(fullPage) {
+        await page.evaluate((viewHeight) => {
+          let screenHeight = document.documentElement.clientHeight;
+          let scrollHeight = document.body.scrollHeight;
+          let screenHeightScrollIncrements = Math.ceil(scrollHeight / screenHeight);
+          var tempScrolledAmount = screenHeight;
+          (function() {
+            var counter = 0;
+            var myVar = setInterval(myTimer, 3000);
+            function myTimer() {
+                window.scrollBy(0, screenHeight);
+                counter++;
+                if(counter > screenHeightScrollIncrements) {
+                  clearInterval(myVar);
+                }
+            }
+          })();
+        });
+      }
+
+      console.log('Waiting for lazy loaded content to appear...');
+      if(fullPage == true) {
         await page.waitFor(30000); // 30,000 confirmed = 30 sec
       } else {
         await page.waitFor(3000); // 3 sec delay for viewport sized screenshots.
@@ -56,10 +59,5 @@ module.exports = {
         }
       });
     })();
-    /*
-    res.status(200).json({
-      message: "Welcome to the backend!"
-    });
-    */
   }
 }
