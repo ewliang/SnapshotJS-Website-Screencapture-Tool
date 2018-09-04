28e1db7257aac927eb010033938be80f0bd0cf27
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-03 23:00:43 -0700

    Html title update.

--- snapshotjs-client/nuxt.config.js
+++ snapshotjs-client/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'snapshotjs-client',
+    title: 'SnapshotJS Website Screencapture Tool by Eric Liang',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
