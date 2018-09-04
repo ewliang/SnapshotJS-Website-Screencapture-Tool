90c01c089fa1e2cd1401d0c345669716a819c62e
Eric Liang <ewliang@users.noreply.github.com>
2018-09-03 23:29:24 -0700

    Updated cors configuration.

--- snapshotjs-server/index.js
+++ snapshotjs-server/index.js
@@ -39,6 +39,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/'));
 
+//Cors Config
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Routes Paths
 app.use('/', index);
 
