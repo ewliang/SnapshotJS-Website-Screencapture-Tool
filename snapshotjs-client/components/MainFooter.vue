f1d6fc4cb667d00695a75f5fc2d17e41053355d7
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-03 23:01:47 -0700

    Updated name with link.

--- snapshotjs-client/components/MainFooter.vue
+++ snapshotjs-client/components/MainFooter.vue
@@ -6,7 +6,7 @@
       <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
       <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
     </form>
-    <span>Copyright &copy; 2018 Eric Liang. Designed and Developed by <a href = "https://www.eric-liang.com">Eric Liang</a>. All Rights Reserved.</span>
+    <span>Copyright &copy; 2018 <a href = "https://www.eric-liang.com">Eric Liang</a>. Designed and Developed by <a href = "https://www.eric-liang.com">Eric Liang</a>. All Rights Reserved.</span>
   </footer>
 </template>
 
