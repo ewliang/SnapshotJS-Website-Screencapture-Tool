7564dcdbb9b0792ffdde45ea4ac954361f9ff6a4
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-03 23:04:36 -0700

    Added copyright.

--- snapshotjs-server/controllers/indexController.js
+++ snapshotjs-server/controllers/indexController.js
@@ -1,3 +1,13 @@
+/*
+* SnapshotJS - indexController.js
+* Designed and Developed by Eric Liang
+* Author: Eric Liang
+* Author URL: https://www.eric-liang.com
+* Author Github: https://www.github.com/ewliang
+* Repository: https://github.com/ewliang/SnapshotJS-Website-Screencapture-Tool
+* License: MIT License
+*/
+
 const puppeteer = require('puppeteer');
 
 module.exports = {
--- snapshotjs-server/index.js
+++ snapshotjs-server/index.js
@@ -1,3 +1,13 @@
+/*
+* SnapshotJS - index.js
+* Designed and Developed by Eric Liang
+* Author: Eric Liang
+* Author URL: https://www.eric-liang.com
+* Author Github: https://www.github.com/ewliang
+* Repository: https://github.com/ewliang/SnapshotJS-Website-Screencapture-Tool
+* License: MIT License
+*/
+
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
